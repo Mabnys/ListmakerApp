622649847cb568fced8ae7404feb3a9d4301bd62
Mamadou Balde <mamadou.balde@its.ny.gov>
2022-09-07 12:47:34 -0400

    Challenge
    RecyclerView => number of items in the list:
    - Create an instance variable called todoLists
    - datatype: Array of Strings
    - Return the size of the array

--- Listmaker/app/src/main/java/com/mamadou/listmaker/TodoListAdapter.kt
+++ Listmaker/app/src/main/java/com/mamadou/listmaker/TodoListAdapter.kt
@@ -4,15 +4,18 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class TodoListAdapter : RecyclerView.Adapter<TodoListViewHolder>() {
+    // Add an instance variable
+    private val todoLists = arrayOf("Android Development", "House Work", "Errands")
+    // This function creates a new View Holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
         TODO("Not yet implemented")
     }
-
+// This function customize the view
     override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
-
+// This function count the Total amount of rows in list
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return todoLists.size // size of our instance variable. Number of elements in the array
     }
 }
